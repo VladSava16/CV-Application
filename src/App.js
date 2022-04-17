88437bffa1b918e770362c4c021964a136697102
Vlad Sava <savavlad82@gmail.com>
2022-04-17 16:47:36 +0300

    Add input + display of personal information

--- src/App.js
+++ src/App.js
@@ -1,1 +1,55 @@
 import React, { Component } from 'react';
+import Header from './components/header';
+import PersonalInfo from './components/PersonalInfo';
+
+export default class App extends Component{
+    constructor(props){
+        super(props);
+
+        this.state = {
+            firstName: "",
+            lastName: "",
+            title: "",
+            address: "",
+            phoneNo: "",
+            email: "",
+            description: "",
+        }
+    }
+    
+    handlePersonalInfo = (formState) => {
+        this.setState({
+            firstName: formState.target.firstName.value,
+            lastName: formState.target.lastName.value,
+            title: formState.target.title.value,
+            address: formState.target.address.value,
+            phoneNo: formState.target.phoneNo.value,
+            email: formState.target.email.value,
+            description: formState.target.description.value,
+        })
+    }
+
+    renderPersonalDetails = () => {
+        return(
+            <div className="personalInfo">
+                <div className="firstName">{this.state.firstName}</div>
+                <div className="lastName">{this.state.lastName}</div>
+                <div className="title">{this.state.title}</div>
+                <div className="address">{this.state.address}</div>
+                <div className="phoneNo">{this.state.phoneNo}</div>
+                <div className="email">{this.state.email}</div>
+                <div className="description">{this.state.description}</div>
+            </div>
+        );
+    }
+
+    render(){
+        return(
+            <div id="content">
+                <Header />
+                <PersonalInfo handlePersonalInformation = {this.handlePersonalInfo}/>
+                {this.renderPersonalDetails()}
+            </div>
+        );
+    }
+}
---
+++ src/components/PersonalInfo.js
@@ -0,0 +1,40 @@
+import React, { Component } from 'react';
+
+export default class PersonalInfo extends Component{
+    constructor(props){
+        super(props);
+        this.state = {
+            firstName: "",
+            lastName: "",
+            title: "",
+            address: "",
+            phoneNo: "",
+            email: "",
+            description: "",
+        }
+    }
+
+    submitData = (e) =>{
+        e.preventDefault();
+        this.props.handlePersonalInformation(e);
+    }
+
+    render(){
+        return(
+            <div className="personalInfo">
+                <p className="sectionHeader">Personal Information</p>
+                <form onSubmit = {this.submitData}>
+                    <input type="text" name="firstName" placeholder="First Name"/>
+                    <input type="text" name="lastName" placeholder="Last Name"/>
+                    <input type="text" name="title" placeholder="Title"/>
+                    <input type="text" name="address" placeholder="Address"/>
+                    <input type="text" name="phoneNo" placeholder="Phone Number"/>
+                    <input type="text" name="email" placeholder="Email"/>
+                    <input type="text" name="description" placeholder="Description"/>
+                    <button type="submit">Submit Section</button>
+                </form>
+            </div>
+        );
+    }
+
+}
